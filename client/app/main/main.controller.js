8e3e2f2b1ab5d889edbc21cfe12f694056cc6d98
kumorko <xkumor@gmail.com>
2015-05-06 14:53:21 +0200

    initial project structure

---
+++ client/app/main/main.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('todosApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [];
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+
+  });
