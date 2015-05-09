404e21ab4a4a0e66b01f4a5fa1d1f7f0c2cfa221
kumorko <xkumor@gmail.com>
2015-05-09 14:50:57 +0200

    base server side validation

--- server/api/task/task.controller.js
+++ server/api/task/task.controller.js
@@ -1,9 +1,11 @@
 'use strict';
-var path = require('path'),
+var _ = require('lodash'),
+    path = require('path'),
+    BadRequestError = require('../../errors/BadRequestError'),
     Datastore = require('nedb'),
     db = new Datastore({
-    	filename: path.join(__dirname, '../../db/tasks.db'),
-    	autoload: true
+        filename: path.join(__dirname, '../../db/tasks.db'),
+        autoload: true
     });
 
 
@@ -13,18 +15,40 @@ exports.index = function(cb) {
 };
 
 exports.show = function(id, cb) {
-	db.findOne({_id: id}, cb);
+    db.findOne({
+        _id: id
+    }, cb);
 };
 
 exports.create = function(data, cb) {
-	data.state = 'todo';
-    db.insert(data, cb);
+    data.state = 'todo';
+    if(!validateData(data)){
+        return cb(new BadRequestError());
+    }
+    db.insert(clearData(data), cb);
 };
 
 exports.update = function(id, data, cb) {
-	db.update({_id: id}, data, {}, cb);
+    if(!validateData(data)){
+        return cb(new BadRequestError());
+    }
+
+    db.update({
+        _id: id
+    }, clearData(data), {}, cb);
 };
 
 exports.destroy = function(id, cb) {
-	db.remove({_id: id}, {}, cb);
+    db.remove({
+        _id: id
+    }, {}, cb);
 };
+
+function clearData(data) {
+    return _.pick(data, ['text', 'state']);
+}
+
+//TODO improve validation
+function validateData(data) {
+    return (data.text && data.text.length > 0 && data.text.length < 100) && (data.state === 'todo' || data.state === 'done');
+}
